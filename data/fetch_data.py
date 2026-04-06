492e5ba9376f2430b4d74ec078251915c8623a3c
Julius Fält Skoglund <julius.faltskoglund@gmail.com>
2026-04-06 21:50:05 +0200

    feat/ test data and api

---
+++ data/fetch_data.py
@@ -0,0 +1,20 @@
+import duckdb
+import requests
+import pandas as pd 
+
+con = duckdb.connect("f1_data.duckdb")
+
+def fetch_openf1(endpoint, params={}):
+    url = f"https://api.openf1.org/v1/{endpoint}"
+    r = requests.get(url, params=params)
+    return pd.DataFrame(r.json())
+
+laps_df = fetch_openf1("laps", {"session_key": 9839})
+drivers_df = fetch_openf1("drivers", {"session_key": 9839})
+weather_df = fetch_openf1("weather", {"session_key": 9839})
+
+con.execute("CREATE OR REPLACE TABLE laps AS SELECT * FROM laps_df")
+con.execute("CREATE OR REPLACE TABLE drivers AS SELECT * FROM drivers_df")
+con.execute("CREATE OR REPLACE TABLE weather AS SELECT * FROM weather_df")
+
+con.close()
